e673145776c18c8d7780f77f62ae2a4d7ea21284
XRay71 <ray200555@gmail.com>
2025-09-13 21:55:55 -0400

    left and right piinch separation

--- Projects/AI Playground/Assets/Entry/PinchDetector.ts
+++ Projects/AI Playground/Assets/Entry/PinchDetector.ts
@@ -1,21 +1,27 @@
+import { GeminiPromptRefiner } from "../_Hack/GeminiPromptRefiner";
 import {PinchService} from './PinchService';
 
 @component
 export class PinchDetector extends BaseScriptComponent {
     @input private pinchService: PinchService;
+    @input defaultTexture: Texture;
+    
+    @input private gemini : GeminiPromptRefiner;
 
     private gestureModule: GestureModule = require('LensStudio:GestureModule');
     onAwake() {
         this.gestureModule
-          .getPinchDownEvent(GestureModule.HandType.Right)
+          .getPinchUpEvent(GestureModule.HandType.Left)
           .add((pinchDownArgs: PinchDownArgs) => {
-            print('Right Hand Pinch Down');
+            print('Undoing capture');
+            this.gemini.inputTexture = this.defaultTexture;
+            this.gemini.setupImageDisplay();
           });
-        
+
         this.gestureModule
-          .getPinchUpEvent(GestureModule.HandType.Right)
+          .getPinchDownEvent(GestureModule.HandType.Left)
           .add((pinchUpArgs: PinchUpArgs) => {
             this.pinchService.pinchDetected();
           });
     }
-}
+}   
